a5e130a79a2bc921905d10ed39f5489cb3c1099b
Bastien Pasdeloup <bastien.pasdeloup@gmail.com>
2023-06-23 14:40:25 +0200

    Added list of dependencies

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup \
     project_urls =                  {"Course" : "https://formations.imt-atlantique.fr/pyrat"},
     license =                       "MIT",
     packages =                      [x[0] for x in os.walk("pyrat")],
-    install_requires =              ["pygame"]
+    install_requires =              ["pygame", "gym", "colored", "distinctipy", "playsound"]
 )
 
 #####################################################################################################################################################
