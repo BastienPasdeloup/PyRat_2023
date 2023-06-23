0fc01f96bd3bbd49bf36c8afdc475736fba67c6f
Bastien Pasdeloup <bastien.pasdeloup@gmail.com>
2023-06-23 18:17:56 +0200

    Corrected setup

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup \
     project_urls =                  {"Course" : "https://formations.imt-atlantique.fr/pyrat"},
     license =                       "MIT",
     package = ["pyrat"],
-    package_data = {"gui" : "pyrat/gui", "workspace" : "pyrat/workspace"},
+    package_data = {"gui" : ["pyrat/gui"], "workspace" : ["pyrat/workspace"]},
     install_requires =              ["pygame", "gym", "colored", "distinctipy", "playsound"]
 )
 
